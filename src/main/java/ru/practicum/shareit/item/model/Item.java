c352775ffbf247d667af12a6c456f18dbb65d7e4
nik110996 <nikitos110996@yandex.ru>
2023-07-20 18:35:32 +0400

    Build a frame110

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
