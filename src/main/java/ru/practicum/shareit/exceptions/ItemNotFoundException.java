c0fb5b4bb7e9bd7a576df3da4f504e2503dbc22a
nik110996 <nikitos110996@yandex.ru>
2023-07-20 17:06:04 +0400

    Build a frame2

--- src/main/java/ru/practicum/shareit/exceptions/ItemNotFoundException.java
+++ src/main/java/ru/practicum/shareit/exceptions/ItemNotFoundException.java
@@ -1,6 +1,9 @@
 package ru.practicum.shareit.exceptions;
 
-public class ItemNotFoundException extends RuntimeException {
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+public class ItemNotFoundException extends IllegalArgumentException {
 
     public ItemNotFoundException(String message) {
         super(message);
--- src/main/java/ru/practicum/shareit/item/ItemErrorHandler.java
+++ src/main/java/ru/practicum/shareit/item/ItemErrorHandler.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.item;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,26 +7,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exceptions.ItemNotFoundException;
 import ru.practicum.shareit.exceptions.UserNotFoundException;
 import ru.practicum.shareit.exceptions.ValidationException;
 
 import java.util.Map;
 
-@RestControllerAdvice("ru.practicum.shareit.user")
-public class UserErrorHandler {
+@RestControllerAdvice("ru.practicum.shareit.item")
+public class ItemErrorHandler {
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleUserNotFound(final UserNotFoundException e) {
-        return Map.of("Ошибка /users", e.getMessage());
+        return Map.of("Ошибка /items", e.getMessage());
     }
+
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleUserNotFound(final ValidationException e) {
-        return Map.of("Ошибка /users", e.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleUserNotFound(final ItemNotFoundException e) {
+        return Map.of("Ошибка /items", e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleUserNotFound(final ValidationException e) {
+        return Map.of("Ошибка /items", e.getMessage());
+    }
+
     @ExceptionHandler
     @ResponseBody
     public ResponseEntity<String> handleValidationException(final MethodArgumentNotValidException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Поля класса User не прошли валидацию");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Поля класса Item не прошли валидацию");
     }
 }
