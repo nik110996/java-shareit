d19ee8530346d221b59781defabb57b7bee3b269
nik110996 <nikitos110996@yandex.ru>
2023-09-04 17:15:39 +0400

    Third Build fix8 04.09.23

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -3,30 +3,29 @@ package ru.practicum.shareit.request.model;
 import lombok.*;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
 @Setter
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor(force = true)
+@NoArgsConstructor
 @Builder
 @Table(name = "requests")
 public class ItemRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NonNull
+    @NotNull
     private String description;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requester_id")
     @ToString.Exclude
     private User requester;
-    @ToString.Exclude
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "request")
+    @OneToMany(mappedBy = "itemRequest", fetch = FetchType.EAGER)
     private List<Item> items;
     private LocalDateTime created;
 }
